97901512ed67b0d59bfee98564993c4a63683721
JuyeongByeon <wndudqus@naver.com>
2021-11-02 00:15:51 +0900

    settings.json 수정, redux 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+
+import { countAdded } from "./app/feature/counterSlice";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const count = useAppSelector(({ counter }) => counter.count);
+  const dispatch = useAppDispatch();
 
   return (
     <div>
       <h1>{count}</h1>
       <h2>{count}</h2>
       <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>++</button>
-      <button onClick={() => setCount(count - 1)}>--</button>
+      <button onClick={() => dispatch(countAdded(count + 1))}>++</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import "./index.css";
 
 import App from "./App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import store from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
